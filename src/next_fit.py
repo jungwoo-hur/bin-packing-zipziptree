6270cac1f17a24bef15f23823e0c985000e480ae
JungWoo Hur <rednef92@gmail.com>
2026-01-29 03:09:16 -0800

    clean up

--- src/next_fit.py
+++ src/next_fit.py
@@ -1,5 +1,4 @@
-# Example file: next_fit.py
-# explanations for member functions are provided in requirements.py
+
 
 def next_fit(items: list[float], assignment: list[int], free_space: list[float]):
 
